b21ea2305d80c515bef5550ac65938049dd6b7bd
MartinKim1954 <kcs19542001@gmail.com>
2020-09-05 01:23:17 +0900

    data handling

--- file_transfer.py
+++ file_transfer.py
@@ -1,3 +1,56 @@
 import os
+import sys
+# PyQt5
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+# Excel
+import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment, Font
+# CSV
+import csv
+# Data
+import pandas as pd
+# infos
+import city_info
 
-item_name = list()
+# MAC
+form_class = uic.loadUiType(
+    "/Users/martinkim/GITHUB/00_Automated System/02_Logistics-team/logistics_tracking-number")
+# Window
+# form_class = uic.loadUiType("")
+
+
+class WindowClass(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btn_import.clicked.connect(self.import_file)
+        self.btn_exit.clicked.connect(self.exit)
+        self.btn_interpret.clicked.connect(self.interpret_file)
+
+    def import_file(self):
+        self.file_name = QFileDialog.getOpenFileName(self)
+        self.tb_import_directory.setText(self.file_name[0])
+
+    def exit(self):
+        QApplication.quit()
+
+    def interpret_file(self):
+        item_name = list()
+        df = pd.read_csv(
+            '/Users/martinkim/GITHUB/00_Automated System/02_Logistics-team/logistics_tracking-number/PO_SKU_LIST_20200904150707.csv')
+
+        # progress bar value changes slowly
+        self.pb_status.setValue(0)
+        self.completed = 0
+        while self.completed < 100:
+            self.completed += 0.0001
+            self.pb_status.setValue(self.completed)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
--- test.py
+++ test.py
@@ -1,28 +1,27 @@
-GWANGJU = {'phone_1': '070-4276-0888', 'phone_2': '070-4276-0888', 'address': '경기도 광주시 도척면 진우리 1006번지 쿠팡 KKW1센터',
-           'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number':  '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-hobeop = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
-HOBEOP = {'phone_1': '070-4234-7009', 'phone_2': '070-4142-7073', 'address': '경기도 이천시 호법면 매곡리 977-5',
-          'quantity': '0', 'item': 'none', 'order_number': '발주번호: ', 'website': '쿠팡'}
+import csv
+import pandas as pd
+
+df = pd.read_csv(
+    '/Users/martinkim/GITHUB/00_Automated System/02_Logistics-team/logistics_tracking-number/PO_SKU_LIST_20200904150707.csv')
+# print(df.shape)
+# print(df.shape[0])
+# goyang_list = []
+# a = df['물류센터'] == '고양1'
+# b = df['발주수량'] >= 10
+# is_goyang_1_and_above_10 = df[a & b]
+# print(is_goyang_1_and_above_10)
+goyang1_order_quantity = []
+goyang1_order_number = []
+is_goyang1 = df[df['물류센터'] == '고양1']
+print(type(is_goyang1.shape[1]))
+for i in range(is_goyang1.shape[1]):
+    goyang1_order_number.append(is_goyang1.iloc[i].loc['발주번호'])
+    goyang1_order_quantity.append(is_goyang1.iloc[i].loc['발주수량'])
+print("고양1 발주번호: ", goyang1_order_number)
+print("고양1 발주수량: ", goyang1_order_quantity)
+
+# for row_index, row in csv_test.iterrows():
+#     # print(row_index)
+#     # print(row)
+#     goyang_list.append(row.loc['물류센터'])
+# print(goyang_list)
